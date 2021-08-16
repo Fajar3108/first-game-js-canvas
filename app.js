48f25faa6fbcfae942e2d9e3a3840a4e3078b08a
Fajar3108 <maulanafajaribrahim@gmail.com>
2021-08-17 06:20:39 +0700

    Enemies

--- app.js
+++ app.js
@@ -44,22 +44,77 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        context.fillStyle = this.color;
+        context.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x += this.velocity.x;
+        this.y += this.velocity.y; 
+    }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue');
 player.draw();
-
 const projectiles = [];
+const enemies = [];
 
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 4) + 4;
+
+        let x = 0;
+        let y = 0;
+
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+            y = Math.random() * canvas.height;
+        } else {
+            x = Math.random() * canvas.width;
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+        }
+
+        const color = 'green';
+
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle),
+        };
+        enemies.push(new Enemy(x, y, radius, color, velocity));
+    }, 1000);
+}
 
 function animate() {
     requestAnimationFrame(animate);
+
     context.clearRect(0, 0, canvas.width, canvas.height);
+
     player.draw();
+
     projectiles.forEach((projectile) => {
         projectile.update();
-    })
+    });
+
+    enemies.forEach((enemy) => {
+        enemy.update();
+    });
 }
 
 window.addEventListener('click', (event) => {
@@ -71,4 +126,5 @@ window.addEventListener('click', (event) => {
     projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity))
 });
 
+spawnEnemies();
 animate();
